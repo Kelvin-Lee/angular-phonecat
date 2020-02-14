a83a976c138a0246721f0c73501f51766855f5e3
Kelvin Lee <Kelvin-Lee@users.noreply.github.com>
2020-02-14 13:40:52 -0800

    step-9 experiments, explanations, comments

--- app/app.config.js
+++ app/app.config.js
@@ -4,6 +4,15 @@ angular.
   module('phonecatApp').
   config(['$routeProvider',
     function config($routeProvider) {
+      /*
+          "Application routes in AngularJS are declared via the 
+           $routeProvider, which is the provider of the $route service. 
+           This service makes it easy to wire together controllers, 
+           view templates, and the current URL location in the browser."
+
+                                                       - Tutorial
+      */
+
       $routeProvider.
         when('/phones', {
           template: '<phone-list></phone-list>'
--- app/app.module.js
+++ app/app.module.js
@@ -2,7 +2,30 @@
 
 // Define the `phonecatApp` module
 angular.module('phonecatApp', [
+  /* 
+     1. The tutorial way where everything works: `ngRoute`, `phoneDetail`, 
+        and `phoneList` are all declared as dependencies to `phonecatApp`
+  */
   'ngRoute',
   'phoneDetail',
   'phoneList'
+
+  /* 
+     2. Experiment: Listing only `phoneDetail` and `phoneList`. This _still_ works, 
+        and I think it's because phoneDetail lists `ngRoute` as a dependency, so 
+        it'll still be loaded, and therefore, we'll be able to access the `$routeProvider` 
+        service in app.config.js.
+  */
+  // 'phoneDetail',
+  // 'phoneList'
+
+  /* 
+     3. Experiment: Listing only `phoneList`. This will not work, since app.config.js will
+        try to use `$routeProvider`, but we neither (1) list ngRoute here in app.module.js as
+        a dependency, nor do we (2) list a dependency that also depends on ngRoute.
+  */
+  // 'phoneList'
+
+  /* Dependency injection is pretty magical, I guess. Best not to try to fiddle with it too much */
+
 ]);
