e507e4b056a1c756afe5f2f34d10f59996bbf8cc
Kelvin Lee <Kelvin-Lee@users.noreply.github.com>
2020-02-13 18:38:25 -0800

    step-2 experiment more

--- app/app.spec.js
+++ app/app.spec.js
@@ -2,13 +2,16 @@
 
 describe('PhoneListController', function() {
 
-  beforeEach(module('phonecatApp'));
+  beforeEach(module('poopapp')); // name of module matters here too heheh
 
-  it('should create a `phones` model with 3 phones', inject(function($controller) {
+  /* I'm kind of guessing that `inject()` returns a function, with whatever is listed in 
+     the params made available to the scope of the returned function? An interesting method */
+  it('should create a `phones` model with 3 phones', inject(function($controller) { 
     var scope = {};
     var ctrl = $controller('PhoneListController', {$scope: scope});
 
     expect(scope.phones.length).toBe(3);
+    expect(scope.fruits.length).toBe(4); // should pass
   }));
 
 });
