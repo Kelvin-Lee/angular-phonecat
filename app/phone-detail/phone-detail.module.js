8be48e66fbb6570846dab407a6854e07a782ee82
Kelvin Lee <Kelvin-Lee@users.noreply.github.com>
2020-02-14 15:38:14 -0800

    step-11 experiments

--- app/phone-detail/phone-detail.module.js
+++ app/phone-detail/phone-detail.module.js
@@ -3,4 +3,8 @@
 // Define the `phoneDetail` module
 angular.module('phoneDetail', [
   'ngRoute'
+   /* 
+      Shouldn't phoneDetail require `'core'` more explicitly for 
+      best practice, since it's leveraging checkmarkFilter? Hmm...
+   */ 
 ]);
