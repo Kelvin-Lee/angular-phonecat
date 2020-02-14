76a9d88bb8cdc3d1a38f951c575b2c9096fb4c32
Kelvin Lee <Kelvin-Lee@users.noreply.github.com>
2020-02-14 12:49:34 -0800

    step-7 experiments, comments, explanations :0

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -5,12 +5,63 @@ angular.
   module('phoneList').
   component('phoneList', {
     templateUrl: 'phone-list/phone-list.template.html',
-    controller: ['$http', function PhoneListController($http) {
-      var self = this;
+    controller: ['$http',  
+    /*
+        ^ I think this string needs to be an exact match of the service's name:
+
+        "Note that the names of arguments are significant, because the injector uses these to look up the dependencies."
+
+                                                                                                           - Tutorial
+
+    */
+          function PhoneListController($http) 
+    {
+      var self = this; // Seems like "best practice" type of thing? In order to reference the controller properly.
       self.orderProp = 'age';
 
-      $http.get('phones/phones.json').then(function(response) {
+      $http.get('phones/phones.json').then(function(response) { // phones/phone.json is relative to index.html
         self.phones = response.data;
+        /* 
+            Based on what the tutorial said, it's attached to a `data` field, as part of AngularJS design:
+
+                "Notice that AngularJS detected the JSON response and parsed it for us into the data 
+                property of the response object passed to our callback!" 
+                                                                                          - Tutorial
+        */
       });
-    }]
+
+      $http.get('fruits/fruits.json').then(function(response) { // fruits/fruit.json is relative to index.html
+        self.fruits = response.data;
+      });
+
+    }] 
+    /*
+        There seems to also be a way to define the `controller:` not as an array, but as a single constructor function.
+        Call the "single-constructor function" method SCF. When utilizing SCF, the code may not/cannot be minified
+        without breaking the dependency injection, since the params passed to the constructor function will get
+        minified.
+
+        See this example, in the tuorial:
+
+            angular.
+              module('phoneList').
+              component('phoneList', {
+                templateUrl: 'phone-list/phone-list.template.html',
+                controller: function PhoneListController($http) { // $http would get minified to something like 'a'
+                  var self = this;
+                  self.orderProp = 'age';
+            
+                  $http.get('phones/phones.json').then(function(response) { // ... But then 'a.get' would not work
+                    self.phones = response.data;
+                  });
+                }
+              });
+
+        Instead, as this file shows, we declare the controller as an array of strings and a single function at the end.
+        The strings are not minified, so we can identify the services we need without the fear that minification will
+        break our application.
+
+        Basically, the _I N J E C T O R_ is the most magical part of Angular.
+
+    */
   });
--- app/phone-list/phone-list.component.spec.js
+++ app/phone-list/phone-list.component.spec.js
@@ -12,8 +12,35 @@ describe('phoneList', function() {
     // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
     // This allows us to inject a service and assign it to a variable with the same name
     // as the service while avoiding a name conflict.
-    beforeEach(inject(function($componentController, _$httpBackend_) {
+
+    /*
+        The injector is MAGIC
+    */
+
+    beforeEach(
+      inject(function($componentController, _$httpBackend_) 
+      /*
+          Regarding `inject()`...
+
+          "NOTE: This function is declared ONLY WHEN running tests with jasmine or mocha"
+
+              - https://docs.angularjs.org/api/ngMock/function/angular.mock.inject
+      */
+    {
       $httpBackend = _$httpBackend_;
+      /*
+          "During testing, this implementation is swapped with _mock_$httpBackend_, which can be
+          trained with responses"
+                                        - https://docs.angularjs.org/api/ng/service/$httpBackend
+
+          So I guess, somehow, the injector knows whether something is a test or not? Hmmmm...
+          Or more likely, only certain services are under ngMock...:
+
+              https://docs.angularjs.org/api/ngMock
+
+          Yeah, probably the latter. At the same time, seems like `inject()` is only exposed when
+          running jasmine or mocha though. HMMMM. MAGIC IN THE BACK.
+      */
       $httpBackend.expectGET('phones/phones.json')
                   .respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
 
