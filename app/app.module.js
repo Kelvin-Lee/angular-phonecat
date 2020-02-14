c55c1ee9c5aa5738bc5b265fb2c6c7dab5cba0a1
Kelvin Lee <Kelvin-Lee@users.noreply.github.com>
2020-02-13 21:08:34 -0800

    step-4 provide comment to explain module dependency

--- app/app.module.js
+++ app/app.module.js
@@ -3,5 +3,19 @@
 // Define the `phonecatApp` module
 angular.module('phonecatApp', [
   // ...which depends on the `phoneList` module
+
+  /*
+      "By passing phoneList inside the dependencies array when defining 
+      the phonecatApp module, AngularJS will make all entities 
+      registered on phoneList available on phonecatApp as well." 
+                                                     - Tutorial
+
+      I guess there's some nuance between the phoneList just being a _component_
+      of phonecatApp, and the phoneList being its own _module_.
+
+      Additionally, now that phoneList is its own module, it can be more easily
+      copy-pasted to some other project (as mentioned by the tutorial). EENTERESTING.
+  */
+
   'phoneList'
 ]);
