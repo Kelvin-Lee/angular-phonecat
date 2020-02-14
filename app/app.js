8f45f3306e68e188e9cb4c57a28f3aeb439f0778
Kelvin Lee <Kelvin-Lee@users.noreply.github.com>
2020-02-13 18:34:06 -0800

    step-2-experiments

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Define the `phonecatApp` module
-var phonecatApp = angular.module('phonecatApp', []);
+var phonecatApp = angular.module('poopapp', []); // `angular` is available because the "<script...lib/angular..." is loaded before the "<script...app.js..."
 
 // Define the `PhoneListController` controller on the `phonecatApp` module
 phonecatApp.controller('PhoneListController', function PhoneListController($scope) {
@@ -16,5 +16,12 @@ phonecatApp.controller('PhoneListController', function PhoneListController($scop
       name: 'MOTOROLA XOOM™',
       snippet: 'The Next, Next Generation tablet.'
     }
+  ];
+
+  $scope.fruits = [
+      {color: 'red'    , name: 'apple'    },
+      {color: 'yellow' , name: 'banana'   },
+      {color: '#AA0000', name: 'cranberry'},
+      {color: '#00AA00', name: 'durian'   }
   ];
 });
